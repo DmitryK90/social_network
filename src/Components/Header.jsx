b70fd04b0d097409370f8c5ae99f807326141791
Dmitry Kachalov <you@example.com>
2023-11-27 20:08:44 +0300

    Commit 1
    Commit 1

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Logo from './../Logo_main.png'
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <img src={Logo}></img>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/Components/Profile.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src="https://media.istockphoto.com/id/1480574526/photo/happy-multigenerational-people-having-fun-sitting-on-grass-in-a-public-park.webp?b=1&s=170667a&w=0&k=20&c=375RBnqauhBmT48mK_W4PvE6bv6dmGhx8F0PJ5HF3so=" alt="" />
+            </div>
+            <div>ava+disk</div>
+            <div>My post
+                <div>New Post</div>
+                <div>
+                    <div>Post1</div>
+                    <div>Post2</div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Profile;
