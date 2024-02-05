bbf09674d0d0c5e7a8c9e1919825adb5905db3e2
Dmitry Kachalov <you@example.com>
2024-02-05 19:40:02 +0300

    94 end

--- src/App.js
+++ src/App.js
@@ -1,56 +1,63 @@
-import React from 'react';
+import React, {Suspense} from 'react';
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
 import Profile from './Components/Profile/Profile';
-import ProfileContainer from './Components/Profile/ProfileContainer';
-import UsersContainer from './Components/Users/UsersContainer'
 import DialogsContainer from './Components/Dialogs/DialogsContainer';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ProfileContainer from './Components/Profile/ProfileContainer';
+// import UsersContainer from './Components/Users/UsersContainer';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import HeaderContainer from './Components/Header/HeaderContainer';
 import LoginPage from './Components/Login/Login';
 import {connect} from "react-redux";
 import {initializeApp} from "./Redux/AppReducer";
 import Preloader from "./Components/common/Preloader/Preloader";
 
+const UsersContainer = React.lazy(() => import('./Components/Users/UsersContainer'));
+// в стартовую загрузку приложения не попадает, подгружается в момент вызова компонента.(входящие в него тоже не подгрузятся.)
+// и обернуть этот компонент в Suspense в return.(в Suspense можно оборачивать несколько компонентов.)
+
 class App extends React.Component {
 
-  componentDidMount() {
-    this.props.initializeApp()
-  }
-
-  render() { // store и state приходят. state - где наши данные. store - функции диспатч и тд.
-
-    if(!this.props.initialized) {
-      return  <Preloader />
+    componentDidMount() {
+        this.props.initializeApp()
     }
 
-    return (
-        <BrowserRouter>
-          <div className='app-wrapper'>
-            <HeaderContainer/>
-            <Navbar/>
-            <div className='app-wrapper-content'>
-              <Routes>
-                <Route path='/dialogs*' element={<DialogsContainer/>}/>
-                {/* <Route path='/profile' element={<ProfileContainer />} /> */}
-                <Route path='/profile/:userId?' element={<ProfileContainer/>}/>
-                <Route path='/users' element={<UsersContainer/>}/>
-                <Route path='/news' element={<Profile/>}/>
-                <Route path='/music' element={<Profile/>}/>
-                <Route path='/settings' element={<Profile/>}/>
-                <Route path='/login' element={<LoginPage/>}></Route>
-              </Routes>
-            </div>
-          </div>
-        </BrowserRouter>
-    ); // /:userId? - параметр, ? - необязательный.
-  }
+    render() { // store и state приходят. state - где наши данные. store - функции диспатч и тд.
+
+        if (!this.props.initialized) {
+            return <Preloader/>
+        }
+
+        return (
+            <BrowserRouter>
+                <div className='app-wrapper'>
+                    <HeaderContainer/>
+                    <Navbar/>
+                    <div className='app-wrapper-content'>
+                        <Routes>
+                            <Route path='/dialogs*' element={<DialogsContainer/>}/>
+                            <Route path='/profile/:userId?' element={<ProfileContainer/>}/>
+                            <Route path='/users' element={
+                                <Suspense fallback={<Preloader/>}>
+                                    <UsersContainer/>
+                                </Suspense>
+                            }/>
+                            <Route path='/news' element={<Profile/>}/>
+                            <Route path='/music' element={<Profile/>}/>
+                            <Route path='/settings' element={<Profile/>}/>
+                            <Route path='/login' element={<LoginPage/>}></Route>
+                        </Routes>
+                    </div>
+                </div>
+            </BrowserRouter>
+        ); // /:userId? - параметр, ? - необязательный.
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    initialized: state.app.initialized
-  }
+    return {
+        initialized: state.app.initialized
+    }
 }
 
 export default connect(mapStateToProps, {initializeApp})(App);
