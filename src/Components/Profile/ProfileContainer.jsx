e3bde2571ba8f0d30a8853a9a31d31e104bc0dce
Dmitry Kachalov <you@example.com>
2024-02-11 22:11:16 +0300

    96 end

--- src/Components/Profile/ProfileContainer.jsx
+++ src/Components/Profile/ProfileContainer.jsx
@@ -1,41 +1,52 @@
 import React from "react";
 import Profile from "./Profile";
 import {connect} from "react-redux";
-import {getStatus, getUserProfile, updateStatus} from '../../Redux/ProfileReducer'
+import {getStatus, getUserProfile, updateStatus, savePhoto} from '../../Redux/ProfileReducer'
 import {useParams} from "react-router-dom";
 import {WithAuthRedirect} from "../../hoc/withAuthRedirect";
 import {compose} from "redux";
 
-export function withRouter(Children) { // Children - передаём компоненту.
+export function withRouter(Children) { // Children - передаём внутрь нашу компоненту.
     return (props) => {
-        const match = {params: useParams()};
-        return <Children {...props} match={match}/>
+        const match = {params: useParams()}; // тут будет id юзера.
+        return <Children {...props} match={match}/> // в ProfileContainer передаём помимо всех props ещё match=id юзера, и возвращаем обновленную компоненту.
     }
 }
 
 class ProfileContainer extends React.Component {
-    componentDidMount() { // метод жизненного цикла.(componentDidMount - компонента уже вмонтирована)
-        let userId = this.props.match.params.userId;
+    refreshProfile() {
+        let userId = this.props.match.params.userId; // id юзера сюда прийдёт.
         if (!userId) {
             userId = this.props.authorizedUserId;
-            if(!userId) {
+            if (!userId) {
                 this.props.history.push('/login');
             }
         }
         this.props.getUserProfile(userId)
         this.props.getStatus(userId)
+    }
 
+    componentDidMount() { // метод жизненного цикла.(componentDidMount - компонента уже вмонтирована)
+        this.refreshProfile();
+    }
 
+    componentDidUpdate(prevProps, prevState, snapshot) {
+        if (this.props.match.params.userId != prevProps.match.params.userId) { // если id юзера изменился, то запускаем refreshProfile.
+            this.refreshProfile();
+        }
     }
 
     render() {
         return (
             <Profile {...this.props}
+                     isOwner={!this.props.match.params.userId} // id юзера в boolean - true/false для отображения фото или заглушки.
+                     savePhoto={this.props.savePhoto} // загрузка фото.
                      profile={this.props.profile}
                      status={this.props.status}
                      updateStatus={this.props.updateStatus}/>
         )
     }// {...this.props} - раскукоживаем все приходящие пропсы и отправляем дальше в Profile.
+    // isOwner - проверка, владелец ли страницы. !! - приводит к булевому true/false.
 }
 
 let mapStateToProps = (state) => ({
@@ -46,7 +57,7 @@ let mapStateToProps = (state) => ({
 })
 
 export default compose(
-    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus}),
+    connect(mapStateToProps, {getUserProfile, getStatus, updateStatus, savePhoto}),
     withRouter,
     WithAuthRedirect
 )(ProfileContainer); // ()() два вызова функции compose, вторые () означают вызов того, что вернули первые(), а не вызов два раза одинакового compose.
